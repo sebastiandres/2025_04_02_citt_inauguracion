83187d27c0c35ac9cfb4cec950b2b063133cb116
Sebastian Flores <sebastian.flores@u-planner.com>
2025-05-30 08:57:02 -0400

    improving taller for uplanner

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import os
 
 pages_list = [
     st.Page(os.path.join("app", "inicio.py"), title="Inicio", icon=":material/home:", url_path="Inicio"),
-    st.Page(os.path.join("app", "tokenizer.py"), title="Tokenizador", icon=":material/robot_2:", url_path="t"),
-    st.Page(os.path.join("app", "next_word.py"), title="Próximo Token", icon=":material/robot_2:", url_path="nt"),
+    #st.Page(os.path.join("app", "tokenizer.py"), title="Tokenizador", icon=":material/robot_2:", url_path="t"),
+    #st.Page(os.path.join("app", "next_word.py"), title="Próximo Token", icon=":material/robot_2:", url_path="nt"),
     st.Page(os.path.join("app", "cittripio_v1.py"), title="Version 1", icon=":material/robot_2:", url_path="v1"),
     st.Page(os.path.join("app", "cittripio_v2.py"), title="Version 2", icon=":material/robot_2:", url_path="vdos"),
     st.Page(os.path.join("app", "cittripio_v3.py"), title="Version 3", icon=":material/robot_2:", url_path="vf"),
